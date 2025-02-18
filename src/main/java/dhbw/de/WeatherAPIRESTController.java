a9640eebc8ca078ac64823479ee537cd95008f4e
Maedthias <kuhnmathias@gmx.net>
2025-02-18 14:51:08 +0100

    push für den fall das server ausfällt; backup jason geändert mit gin

--- src/main/java/dhbw/de/WeatherAPIRESTController.java
+++ src/main/java/dhbw/de/WeatherAPIRESTController.java
@@ -8,12 +8,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.server.ConfigurableWebServerFactory;
 import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
 
 @RestController
 @SpringBootApplication(scanBasePackages = "dhbw.de")
@@ -56,7 +53,7 @@ public class WeatherAPIRESTController {
         logger.info("GET-Request auf /get_station empfangen!");
         return stationService.stationSearch(lat, lon, radius).toString();
 
-        // Path path = Path.of("station-data.json");
+        //Path path = Path.of("station_data_test.json");
         //String jsonContent = Files.readString(path);
 
         //return ResponseEntity.ok(jsonContent);
@@ -66,8 +63,8 @@ public class WeatherAPIRESTController {
     public String getWeatherData(@RequestParam String stationId, @RequestParam int startYear, @RequestParam int endYear) throws IOException {
         logger.info("GET-Request auf /get_weather_data empfangen!");
         return weatherDataService.fetchAndProcessWeatherDataByYear(stationId, startYear, endYear);
-        //Path path = Path.of("weather_data.json");
-        // String jsonContent = Files.readString(path);
+        //Path path = Path.of("weather_data_test.json");
+        //String jsonContent = Files.readString(path);
 
         //return ResponseEntity.ok(jsonContent);
     }
